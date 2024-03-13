fa662d19188ad487eaef5796296c67efcc611cb9
benjaminavachon <111586627+benjaminavachon@users.noreply.github.com>
2024-03-13 17:15:48 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask import request
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/team<id>')
+@app.route('/team/<id>')
 def team_id(id):
     return id
